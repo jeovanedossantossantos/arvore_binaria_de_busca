dc5056fa4ea16faf4f49392eb9001b3260ccc123
jeovanedossantossantos <jeovane.ssantos@outlook.com>
2023-05-14 20:30:31 -0300

    feat: init projeto

---
+++ func.h
@@ -0,0 +1,17 @@
+#ifndef LIBPRINT_H
+#define LIBPRINT_H
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+
+#include <algorithm>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <vector>
+
+using namespace std;
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "func.cpp"
+using namespace std;
+
+int main()
+{
+
+    do
+    {
+
+        switch (1)
+        {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        default:
+            break;
+        }
+
+    } while (1 != 0);
+
+    return 0;
+}
